176bfa36b589409f99139a28846d590958037389
NeocortexF <eugeneshamkin@mail.ru>
2017-03-14 09:04:07 +0500

    Chapter 4 sources and homework

---
+++ Laf4/src/dz/Queue.java
@@ -0,0 +1,67 @@
+package dz;
+
+class Queue {
+
+    private int maxSize;
+    private long[] queArray;
+    private int front;
+    private int rear;
+    private int nItems;
+
+    public Queue(int s) {
+        maxSize = s;
+        queArray = new long[maxSize];
+        front = 0;
+        rear = -1;
+        nItems = 0;
+    }
+
+    public void insert(long j) // put item at rear of queue
+    {
+        if (rear == maxSize - 1) // deal with wraparound
+        {
+            rear = -1;
+        }
+        queArray[++rear] = j;         // increment rear and insert
+        nItems++;                     // one more item
+    }
+
+    public long remove() // take item from front of queue
+    {
+        long temp = queArray[front++]; // get value and incr front
+        if (front == maxSize) // deal with wraparound
+        {
+            front = 0;
+        }
+        nItems--;                      // one less item
+        return temp;
+    }
+
+    public long peekFront() // peek at front of queue
+    {
+        return queArray[front];
+    }
+
+    public boolean isEmpty() // true if queue is empty
+    {
+        return (nItems == 0);
+    }
+
+    public boolean isFull() // true if queue is full
+    {
+        return (nItems == maxSize);
+    }
+
+    public int size() // number of items in queue
+    {
+        return nItems;
+    }
+    
+    // написать метод показывающий содержимое очереди
+    public int showQueue() {
+        for (int i = queArray.length - 1; i > 0 ; i--) {
+            System.out.println("Queue: " + queArray[i]);
+        }
+        return 0;
+    }
+}
